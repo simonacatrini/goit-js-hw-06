64a8ceb2aa6c8ed9fe0959fc2e79f5ee5afab7b7
simonacatrini <simoacatrini12@yahoo.com>
2024-06-17 11:48:50 +0300

    c12

--- js/exercitiu-9.js
+++ js/exercitiu-9.js
@@ -3,15 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const showColor = document.querySelector(".color");
-const btn = document.querySelector(".change-color");
-// document.querySelector(".btn").addEventListener("click", function () {
-//   document.body.style.backgroundColor = getRandomHexColor();
-// });
 
-btn.addEventListener("click", function onClick() {
-  const body = document.querySelector("body");
+const changeColor = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+
+changeColor.addEventListener("click", handleEvent);
+
+function handleEvent() {
   const randomColor = getRandomHexColor();
-  body.style.backgroundColor = randomColor;
-  showColor.textContent = randomColor;
-});
+  document.body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
+}
